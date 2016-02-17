c7b91c084dabd8e4a05a5123ab874c22fc307041
Sonya Trachsel <sofyatrachsel@gmail.com>
2016-02-16 20:06:50 -0800

    changed filmTemplate.js to add proper Hanlebars display function

--- filmTemplate.js
+++ filmTemplate.js
@@ -1,15 +1,20 @@
 $(function(){
   // Extract the text from the template
-  var displayTemplate = $('#filmTemplate').html();
+  var displayTemplate = $('#film-template').html();
   // Complile that into an handlebars template
   var template = Handlebars.compile(displayTemplate);
   // Retrieve the placeHolder where the posts will be displayed
-  var placeHolder = $('#placeHolder');
+  var data = $.getJSON( "festivalData.json", function(data) {
+    var films = [];
+    $.each(data, function( key, val ) {
+      films.append( "<li id='" + key + "'>" + val + "</li>" );
+  });
+  $('#placeHolder').html(template(data));
   // Fetch all portfolio items data from the server in JSON
-  $.get('festivalData.json', function(data){
-     // Generate the HTML for each post+
-     var html = template({data});
-       // Render the posts into the page
-     placeHolder.append(html);
+  // $.get('festivalData.json', function(data){
+  //    // Generate the HTML for each post+
+  //  var html = template({portfolio:data});
+  //    // Render the posts into the page
+  //  placeHolder.append(html);
  });
 });

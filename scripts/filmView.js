8bd631fe81b04508b8998fcd5d998ac9ca0acb79
Allison Davis <a.rae.davis@gmail.com>
2016-02-17 20:21:04 -0800

    add remaining filter population methods

--- scripts/filmView.js
+++ scripts/filmView.js
@@ -7,16 +7,56 @@
     var template = Handlebars.compile($('#filter-template').text());
 
     Film.allDates(function(rows){
-      $('#date-filter').append(
-        rows.map(function(row){
-          return template({val: row.datetime});
-        })
-      );
+      if($('#date-filter option').length < 2) {
+        $('#date-filter').append(
+            rows.map(function(row){
+              return template({
+                val: row.datetime });
+            })
+          );
+      };
     });
-  }
+
+    Film.allVenues(function(rows){
+      if($('#venue-filter option').length < 2) {
+        $('#venue-filter').append(
+            rows.map(function(row){
+              return template({
+                val: row.venue });
+            })
+          );
+      };
+    });
+
+    Film.allCountries(function(rows){
+      if($('#country-filter option').length < 2) {
+        $('#country-filter').append(
+            rows.map(function(row){
+              return template({
+                //returns row.country converted to title case
+                val: (row.country).replace(/\w\S*/g, function(txt){
+                  return txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase();
+                }) });
+            })
+          );
+      };
+    });
+
+    Film.allGenres(function(rows){
+      if($('#venue-filter option').length < 2) {
+        $('#venue-filter').append(
+            rows.map(function(row){
+              return template({
+                val: row.genre });
+            })
+          );
+      };
+    });
+  };
 
   //test function calls
   filmView.populateFilters();
+
 
   module.filmView = filmView;
 })(window);

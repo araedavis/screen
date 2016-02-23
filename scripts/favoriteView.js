c1670548ea1b070b111599f9c0435b87b46fdc27
Allison Davis <a.rae.davis@gmail.com>
2016-02-22 20:57:28 -0800

    clean-up

--- scripts/favoriteView.js
+++ scripts/favoriteView.js
@@ -3,22 +3,27 @@
 
   var favoriteView = {};
 
+
+  // favoriteView.addTwitterWidget = function(){
+  //   var template = Handlebars.compile($('#twitter-template').text());
+  //   return template;
+  // };
+
   favoriteView.initMyFilms = function(){
     Film.fetchAllFilmData(function(returnedArray){
       return returnedArray.filter(function(film){
         //if film is favorited
-        console.log(typeof film.isFavorite);
         return (film.isFavorite === 'true');
       })
       .forEach(function(element){
+        //var completedFilm = $(element).append(favoriteView.addTwitterWidget());
+
         $('#my-films').append(filmView.render(element));
       });
     });
   };
 
-  favoriteView.addTwitterWidget = function(){
 
-  };
 
 
   module.favoriteView = favoriteView;
